f200f53a81757d3ed0121e9d16bad4a4c8c82adc
Anasmb <40839455+Anasmb@users.noreply.github.com>
2022-07-21 22:10:45 +0300

    Bug Fixes

--- Task2/SpeechCommand.ino
+++ Task2/SpeechCommand.ino
@@ -4,9 +4,6 @@ Servo myservo;
 
 int servoPosition = 0; 
 const int servoPin = 9;
-const int BUILTIN_LED = 2;
-bool toggleBlink = true;
-int blinkDelay = 1000;
 
 void setup() {
   Serial.begin(9600);
@@ -17,43 +14,32 @@ void setup() {
     delay(1000);
     digitalWrite(LED_BUILTIN,LOW);
    myservo.attach(servoPin);
-
+   
+  myservo.write(90);  // center the servo
   Serial.println("Begin");
 
 }
 
 void loop() {
   
-
     if(Serial.available() > 0){
         String incomingString = Serial.readString();
         Serial.print(F("Received from Serial: ")); Serial.println(incomingString);
-      if(incomingString.startsWith("right")){
-        Serial.println(F("Turning Right"));
+        if(incomingString.startsWith("right")){
+          Serial.println(F("Turning Right"));
           for (servoPosition = 0; servoPosition <= 180; servoPosition++) {
                  myservo.write(servoPosition);              
                  delay(20);                     
           } 
-      }
-      else if(incomingString.startsWith("left")){
-        Serial.println(F("Turning Left"));
+        }
+     else if(incomingString.startsWith("left")){
+         Serial.println(F("Turning Left"));
          for (servoPosition = 180; servoPosition >= 0; servoPosition--) { 
              myservo.write(servoPosition);            
             delay(20);                      
-           }
-       
+         }    
       }
-      else if(incomingString.startsWith("stop")){
-       
-      
-        }
-      
     }
-
-
- 
-
-  
     
 
 }
