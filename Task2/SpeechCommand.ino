379b72804199de1ce4daf774fc2ea79d86052dcb
Anasmb <40839455+Anasmb@users.noreply.github.com>
2022-07-21 21:33:15 +0300

    + added arduino code file, added Javascript serial API code

---
+++ Task2/SpeechCommand.ino
@@ -0,0 +1,59 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int servoPosition = 0; 
+const int servoPin = 9;
+const int BUILTIN_LED = 2;
+bool toggleBlink = true;
+int blinkDelay = 1000;
+
+void setup() {
+  Serial.begin(9600);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN,LOW);
+   myservo.attach(servoPin);
+
+  Serial.println("Begin");
+
+}
+
+void loop() {
+  
+
+    if(Serial.available() > 0){
+        String incomingString = Serial.readString();
+        Serial.print(F("Received from Serial: ")); Serial.println(incomingString);
+      if(incomingString.startsWith("right")){
+        Serial.println(F("Turning Right"));
+          for (servoPosition = 0; servoPosition <= 180; servoPosition++) {
+                 myservo.write(servoPosition);              
+                 delay(20);                     
+          } 
+      }
+      else if(incomingString.startsWith("left")){
+        Serial.println(F("Turning Left"));
+         for (servoPosition = 180; servoPosition >= 0; servoPosition--) { 
+             myservo.write(servoPosition);            
+            delay(20);                      
+           }
+       
+      }
+      else if(incomingString.startsWith("stop")){
+       
+      
+        }
+      
+    }
+
+
+ 
+
+  
+    
+
+}
