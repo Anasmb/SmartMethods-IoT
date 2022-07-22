8c5c28ebe8c72d1aa30f76ba950f57b246d7e4a7
Anasmb <40839455+Anasmb@users.noreply.github.com>
2022-07-22 17:58:01 +0300

    added radio buttons

--- Task2/SpeechCommand.ino
+++ Task2/SpeechCommand.ino
@@ -2,44 +2,51 @@
 
 Servo myservo;
 
-int servoPosition = 0; 
+int servoPosition = 0;
 const int servoPin = 9;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-    pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-    digitalWrite(LED_BUILTIN,HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN,LOW);
-   myservo.attach(servoPin);
-   
-  myservo.write(90);  // center the servo
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  myservo.attach(servoPin);
+
+  myservo.write(90); // center the servo
+  Serial.println("© Copyright 2022 - Written By Anasmb");
   Serial.println("Begin");
-
 }
 
-void loop() {
-  
-    if(Serial.available() > 0){
-        String incomingString = Serial.readString();
-        Serial.print(F("Received from Serial: ")); Serial.println(incomingString);
-        if(incomingString.startsWith("right")){
-          Serial.println(F("Turning Right"));
-          for (servoPosition = 0; servoPosition <= 180; servoPosition++) {
-                 myservo.write(servoPosition);              
-                 delay(20);                     
-          } 
-        }
-     else if(incomingString.startsWith("left")){
-         Serial.println(F("Turning Left"));
-         for (servoPosition = 180; servoPosition >= 0; servoPosition--) { 
-             myservo.write(servoPosition);            
-            delay(20);                      
-         }    
+void loop()
+{
+
+  if (Serial.available() > 0)
+  {
+    String incomingString = Serial.readString();
+    Serial.print(F("Received from Serial: "));
+    Serial.println(incomingString);
+    if (incomingString.startsWith("right"))
+    {
+      Serial.println(F("Turning Right"));
+      for (servoPosition = 0; servoPosition <= 180; servoPosition++)
+      {
+        myservo.write(servoPosition);
+        delay(20);
       }
     }
-    
-
+    else if (incomingString.startsWith("left"))
+    {
+      Serial.println(F("Turning Left"));
+      for (servoPosition = 180; servoPosition >= 0; servoPosition--)
+      {
+        myservo.write(servoPosition);
+        delay(20);
+      }
+    }
+  }
 }
+//   © Copyright 2022 - Written By Anasmb
